776ac9c78dce0aba1c2b9ab9921f51e314c24161
Ami Falk <ami.falk@lmi.org>
2022-10-22 15:58:30 -0400

    setting up config file

---
+++ config/config.py
@@ -0,0 +1,9 @@
+TSIR_CONFIG = {
+    "iters":75,    # number of iterations to run sim for
+    "tau1":0.7,    # gravity model parameters
+    "tau2":1.2,
+    "rho": 0.97,
+    "theta": 0.05,
+    "alpha": 0.97, # mixing rate
+    "beta": 7      # disease infectiousness
+}
